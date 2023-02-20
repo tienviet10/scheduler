84815754c6d36d558f786eb10b1e1540fb1682a6
Viet Tran <viettran101294@gmail.com>
2023-02-20 17:31:59 -0500

    add classnames to style button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import React from "react";
-
+import classNames from "classnames";
 import "components/Button.scss";
 
 export default function Button(props) {
-  return <></>;
+  return <button disabled={props.disabled} className={classNames('button', { 'button--confirm': props.confirm, 'button--danger': props.danger })} onClick={props.onClick}>{props.children}</button>;
 }
